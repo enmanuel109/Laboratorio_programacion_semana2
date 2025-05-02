0507068eac81a1a26ff294aeeba162b5bbc40a0e
enmanuel109 <enmanuelcantarer001@gmail.com>
2025-05-02 10:03:41 -0600

    adelanto del primero, inicio del otro

--- Laboratorio_programacion2/src/Ejercicios_Laboratorio_semana2/Cantarero_SalvadorClases.java
+++ Laboratorio_programacion2/src/Ejercicios_Laboratorio_semana2/Cantarero_SalvadorClases.java
@@ -3,11 +3,31 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Ejercicios_Laboratorio_semana2;
-
+import java.util.Scanner;
 /**
  *
  * @author Cantarero
  */
 public class Cantarero_SalvadorClases {
-    
+    public static void main(String[] args) {
+        Scanner lea = new Scanner(System.in);
+        lea.useDelimiter("/n");
+        
+        System.out.print("Ingrese la fecha actual(dia/DD/MM):");
+        String fecha = lea.nextLine(); 
+        
+        String dia = fecha.substring(0, fecha.indexOf("/"));
+        String dia_del_mes = fecha.substring(fecha.indexOf("/"), fecha.indexOf("/"));
+        String Mes = fecha.substring(fecha.indexOf("/"));
+        int dia_mes = Integer.parseInt(dia_del_mes);
+        int mes = Integer.parseInt(Mes);
+        
+        if (!dia.equals("lunes")|| !dia.equals("martes") ||!dia.equals("miercoles") || !dia.equals("jueves") || !dia.equals("viernes")|| !dia.equals("sabado")||!dia.equals("domingo")){
+            System.out.println("ERROR: el dia que ingreso de la semana es incoreecto.");
+        } else if (dia_mes <1 || dia_mes>30) {
+            System.out.println("ERROR: el dia que ingreso del mes es incoreecto.");
+        } else if ( mes<1 || mes>12){
+            System.out.println("ERROR: el mes que ingreso es incoreecto.");
+        }
+ }
 }
