262d5bdb59d907b0e2b03b6c9931b018b7e47b49
enmanuel109 <enmanuelcantarer001@gmail.com>
2025-05-02 10:50:14 -0600

    empecé con el cuarto y ya estoy en proceso de terminarlo

---
+++ Laboratorio_programacion2/src/Ejercicios_Laboratorio_semana2/Cantarero_SalvadorFecha.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ejercicios_Laboratorio_semana2;
+import java.util.Scanner;
+/**
+ *
+ * @author Cantarero
+ */
+public class Cantarero_SalvadorFecha {
+    public static void main(String[] args) {
+            Scanner lea = new Scanner(System.in);
+            
+            System.out.println("ingresar la primera fecha(DIA/MES/AÑO):");
+            int primera_fecha = lea.nextInt();
+                                
+            System.out.println("ingresar la segunda fecha(DIA/MES/AÑO):");
+            int segunda_fecha = lea.nextInt();
+    
+            int primeros_dia = Integer.parseInt(primeros_dia);
+    }
+
+    
+
+    
+}
