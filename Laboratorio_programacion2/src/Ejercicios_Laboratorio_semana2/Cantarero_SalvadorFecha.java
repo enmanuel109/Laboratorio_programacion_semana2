0fd69b0a87a2f230e16a04e9d5ce368e5282149f
enmanuel109 <enmanuelcantarer001@gmail.com>
2025-05-02 11:05:18 -0600

    termine el ejercicio numero 4

--- Laboratorio_programacion2/src/Ejercicios_Laboratorio_semana2/Cantarero_SalvadorFecha.java
+++ Laboratorio_programacion2/src/Ejercicios_Laboratorio_semana2/Cantarero_SalvadorFecha.java
@@ -12,16 +12,27 @@ public class Cantarero_SalvadorFecha {
     public static void main(String[] args) {
             Scanner lea = new Scanner(System.in);
             
-            System.out.println("ingresar la primera fecha(DIA/MES/AÑO):");
-            int primera_fecha = lea.nextInt();
+            String text1 = ("ingresar la primera fecha(DIA/MES/Ano):");
+            System.out.print(text1);
+
+            String primera_fecha = lea.nextLine();
                                 
-            System.out.println("ingresar la segunda fecha(DIA/MES/AÑO):");
-            int segunda_fecha = lea.nextInt();
+            System.out.print("ingresar la segunda fecha(DIA/MES/Ano):");
+            String segunda_fecha = lea.nextLine();
     
-            int primeros_dia = Integer.parseInt(primeros_dia);
+            int primeros_dias = Integer.parseInt(primera_fecha.substring(0, 2));
+            int primeras_meses = Integer.parseInt(primera_fecha.substring(3, 5));
+            int primeros_años = Integer.parseInt(primera_fecha.substring(6));       
+            int segundos_dias = Integer.parseInt(segunda_fecha.substring(0, 2));
+            int segundas_meses = Integer.parseInt(segunda_fecha.substring(3, 5));
+            int segundos_años = Integer.parseInt(segunda_fecha.substring(6)); 
+            
+            int total_de_dia1 = primeros_dias + (primeras_meses * 30) + (primeros_años * 360);
+            int total_de_dia2 = segundos_dias + (segundas_meses * 30) + (segundos_años * 360);
+            
+            int total_dias =(total_de_dia2 - total_de_dia1);
+
+            String text = "La diferencia entre las fechas es de " + total_dias + " dias.";
+            System.out.println(text);
     }
-
-    
-
-    
 }
